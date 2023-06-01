bf3f01a339537e7e0e0ecbaf207d57dac7e7ebe9
feddebh <bluisfederico@gmail.com>
2023-06-01 02:01:16 -0300

    new:  started new project

---
+++ src/main/java/com/bohle/checklistservice/ChecklistserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bohle.checklistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bohle/checklistservice/ChecklistserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohle.checklistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
