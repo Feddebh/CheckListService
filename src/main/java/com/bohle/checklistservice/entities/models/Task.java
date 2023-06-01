26080ea95b972e9e6752f89b0f46aaf9e7e33c0b
feddebh <bluisfederico@gmail.com>
2023-06-01 03:01:27 -0300

    modified: pom.xml(added jackson dependency), models(added attributes)

--- src/main/java/com/bohle/checklistservice/entities/models/Task.java
+++ src/main/java/com/bohle/checklistservice/entities/models/Task.java
@@ -1,4 +1,37 @@
 package com.bohle.checklistservice.entities.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Data
+
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("task_id")
+    private Long id;
+
+    @Column(name= "task_title")
+    private String taskTitle;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate deadLine;
+
+    @Column(nullable = false)
+    private String status;
+
+    // Relación muchos a uno con la entidad Usuario
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
 }
--- src/main/java/com/bohle/checklistservice/entities/models/User.java
+++ src/main/java/com/bohle/checklistservice/entities/models/User.java
@@ -1,7 +1,29 @@
 package com.bohle.checklistservice.entities.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> tasks;
 
 }
