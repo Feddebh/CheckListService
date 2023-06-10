7ea27e1b78064ffbc259de3e5c526c44d346040c
feddebh <bluisfederico@gmail.com>
2023-06-10 11:50:54 -0300

    added generated-sources file in target

---
+++ target/generated-sources/annotations/com/bohle/checklistservice/mappers/UserMapperImpl.java
@@ -0,0 +1,33 @@
+package com.bohle.checklistservice.mappers;
+
+import com.bohle.checklistservice.entities.dto.UserDTO;
+import com.bohle.checklistservice.entities.models.User;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-10T11:24:06-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 11.0.19 (Ubuntu)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User userDtoToUser(UserDTO userDTO) {
+        if ( userDTO == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        return user;
+    }
+
+    @Override
+    public void updateUserFromDto(UserDTO userDTO, User user) {
+        if ( userDTO == null ) {
+            return;
+        }
+    }
+}
